87cdca7af3beef9127add08570c384962e11f6d3
KhvanVladimir <khvan.vi0@gmail.rom>
2022-12-20 20:19:16 +0900

    Исправил Needle7 (сделал конструктор)

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -6,10 +6,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class Needle7 {
 
-    private Deth8 deth;
+    private final Deth8 deth;
 
     @Autowired
-    public void Needle7(Deth8 deth) {this.deth = deth;}
+    public Needle7(Deth8 deth) {this.deth = deth;}
 
     @Override
     public String toString() {
